995f929e44698b4031ea248700eb44dc58405093
arnav <arnavagarwal@urbancompany.com>
2022-05-25 00:16:47 +0530

    pushed vs present difference

--- PassingDataDemo/DummyController.swift
+++ PassingDataDemo/DummyController.swift
@@ -18,4 +18,16 @@ class DummyController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func button(_ sender: UIButton) {
+        if let resultVC = (storyboard?.instantiateViewController(identifier: "screen2Controller"))! as Screen2Controller? {
+//            resultVC.dummyString = "Hello"
+            // 1. this will work if this controller is either pushed or if it is shown modally , then this controller is set as the root of the navigation controller. Otherwise, if this controller is simply presented, then the following statement wont work.
+            navigationController?.pushViewController(resultVC, animated: true)
+            
+            // 2. this can be used to present the new controller. But this will work , only if the current controller is pushed onto the existing navigation controller.
+//            self.present(resultVC, animated: true, completion: {
+//                print("yes. done")
+//            })
+        }
+    }
 }
--- PassingDataDemo/ViewController.swift
+++ PassingDataDemo/ViewController.swift
@@ -19,7 +19,17 @@ class ViewController: UIViewController {
         
         if let resultVC = (storyboard?.instantiateViewController(identifier: "dummyController"))! as DummyController? {
             resultVC.dummyString = "Hello"
-            navigationController?.pushViewController(resultVC, animated: true)
+            // 1. this is simply pushing the new controller on top of this one. Hence, push or present will both work in it.
+//            navigationController?.pushViewController(resultVC, animated: true)
+            
+            // 2. If this is done, then new controller(shown as modally) cannot present or push new controllers using navigation controller.
+//            self.present(resultVC, animated: true, completion: {
+//                print("yes. done")
+//            })
+            
+            // 3. setting new controller as the root of the navigation controller rather than the current controller. In this way , the new controller (shown as modally ) will be able to have a navigation stack of its own.
+            let nav = UINavigationController(rootViewController: resultVC )
+            self.present(nav, animated:true, completion:nil)
         }
     }
 }
